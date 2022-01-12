2b492d27d5aef0ed097fcb69a4a0253819b58db1
Albert <gooferstotjyu@yandex.ru>
2022-01-12 08:40:21 +0600

    create

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <div>one div</div>
+    <div className="App">
+      <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>javascript</li>
+        <li>react</li>
+      </ul>
+    </div>
+    </div>
+  );
+}
+
+export default App;
