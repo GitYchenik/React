eaa813b6b13293020efc096be5d14058bd7689b3
Albert <gooferstotjyu@yandex.ru>
2022-01-15 01:06:18 +0600

    изменеие функции на отдельные компаненты

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div>
-      <div><Header></Header></div>
-      <div className="App">
-        <ul>
-          <li>html</li>
-          <li>css</li>
-          <li>javascript</li>
-          <li>react</li>
-        </ul>
-      </div>
+      <Header/>
+      <Technologies/>
     </div>)
 };
 
-      const Header = () => {
-        return (<div>
-          <h4><a href="#s">Home</a></h4>
-          <h4><a href="#s">New Feeds</a></h4>
-          <h4><a href="#s">Messages</a></h4>
-        </div>)
-      };
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>javascript</li>
+        <li>react</li>
+      </ul>
+    </div>
+  )
+}
+
+const Header = () => {
+  return (<div>
+    <h4><a href="#s">Home</a></h4>
+    <h4><a href="#s">New Feeds</a></h4>
+    <h4><a href="#s">Messages</a></h4>
+  </div>)
+};
 
 
 export default App;
