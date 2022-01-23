81364112c136a6d040741fe3b48a3cf4f0ffdb33
Albert <gooferstotjyu@yandex.ru>
2022-01-24 02:18:32 +0600

    Update 24/01/2022 -дабавден импорт отдельного css модуля для гибкости оформления -изменения класса для подключения модуля

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
+import classes from './style/NavBar.module.css';
 //компонента левого бокового меню
 
 const NavBar = () => {
     return (
-        <nav className='nav'>
-            <div>
+        <nav className={classes.nav}>   {/*это необходимо что бы я перешел в класс(путь который прописан выше) и перешел в свойство .nav */}
+            <div className={`${classes.item} ${classes.item_active}`}>  {/*Аналогично перешел в класс(путь который прописан выше) и перешел в свойство .item */}
                 <a>Profile</a>
             </div>
-            <div>
+            <div className={classes.item}>
                 <a>Messages</a>
             </div>
-            <div>
+            <div className={classes.item}>
                 <a>News</a>
             </div>
-            <div>
+            <div className={classes.item}>
                 <a>Music</a>
             </div>
-            <div>
+            <div className={classes.item}>
                 <a>Setting</a>
             </div>
         </nav>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import classes from './style/Profile.module.css'
 //основная страница правая нижняя 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={classes.content}>
             <img src='https://sun9-35.userapi.com/yqDTUSL01tK_rlXAoJlI7lKESP82QBdS9uZVWA/DP8DugK6zYs.jpg'></img>
 
             <div>Avatar+description</div>
             <div>My post</div>
             <div>New post</div>
-            <div>post1</div>
+            <div className={classes.item}>post1</div>
             <div>post2</div>
             <div>post3</div>
         </div>
