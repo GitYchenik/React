f0ea19cd1c1f91ea57210152e43c1aafcfb9f1cf
Albert <gooferstotjyu@yandex.ru>
2022-01-24 02:15:33 +0600

    Update 23/01/2022 -балуюсь (попытки вставить аудио для настроения :)))

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import React from 'react';
 import Header from './components/Header.jsx';
 import NavBar from './components/NavBar.jsx';
 import Profile from './components/Profile.jsx';
+import Audio from './components/sound';
 const App = () => {
   return (
     <div className='app-wrapper'>
+      <Audio></Audio>
       <Header />
       <NavBar />
       <Profile />
