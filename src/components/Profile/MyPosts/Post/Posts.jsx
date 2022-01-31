b6cb8203cdca7bafefde5714c1ef6b745d05b6a2
Albert <gooferstotjyu@yandex.ru>
2022-02-01 02:12:49 +0600

    Update 01/02/2022 -заменил текст на props.message

--- src/components/Profile/MyPosts/Post/Posts.jsx
+++ src/components/Profile/MyPosts/Post/Posts.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import classes from './Posts.module.css'
 //комментарии 
-const Post = () => {
+const Post = (props) => {
     return (
         <div>
             <div className={classes.item}>
                 <img src='https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg'></img>
-                post1</div>
+                {props.message}</div>
                 <div>
                     <span>Like</span>
                 </div>
