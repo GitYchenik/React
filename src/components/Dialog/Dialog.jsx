64acfdaccada42b5beacf010d9564885a0a4b557
Albert <gooferstotjyu@yandex.ru>
2022-02-08 00:33:51 +0600

    Update 08/02/2022 -Dialog.jsx создали два раздела пользователи и сообщения -добавили стили в dialog.css -изменили размер изображения в px на странице в profile.css -в navbar создан отступ

--- src/components/Dialog/Dialog.jsx
+++ src/components/Dialog/Dialog.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import style from './Dialog.module.css'
 
-const Dialog = ()=> {
-    return(
-        <div className={style.content}>
-            <div>Hello!!!</div>
+const Dialogs = () => {
+    return (
+        <div className={style.dialogs}>
+            <div className={style.dialogItems}>
+                <div className={style.dialog + ' ' + style.active}>
+                    Dimach
+                </div>
+                <div className={style.dialog}>
+                    Albert
+                </div>
+                <div className={style.dialog}>
+                    Parker
+                </div>
+            </div>
+
+            <div className={style.messages}>
+                <div className={style.message}>Hi</div>
+                <div className={style.message}>I am Progromist</div>
+                <div className={style.message}>I am Peter Parker</div>
+            </div>
+
         </div>
     )
 };
 
-export default Dialog;
+export default Dialogs;
