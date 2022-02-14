7e8405c8bad89b2a3966f9a2811f9ef13bd516b1
Albert <gooferstotjyu@yandex.ru>
2022-02-15 00:51:48 +0600

    -Update 15/02/2022 удален ссылка на импорт sound.js (времено отказался от идеи звука)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import React from 'react';
 import Header from './components/Header/Header.jsx';
 import NavBar from './components/NavBar/NavBar.jsx';
 import Profile from './components/Profile/Profile.jsx';
-import Audio from './components/songs/sound.js';
 import Dialog from './components/Dialog/Dialog.jsx';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
