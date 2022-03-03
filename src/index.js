b05d306fa7ebc7111d1f21c69b7b0985892d4160
Albert <gooferstotjyu@yandex.ru>
2022-03-03 16:52:51 +0600

    Update 03/03/2022 Вынесены данные в верх на уровень

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let posts = [
+  { id: 1, message: 'It My first post', likesCount: 0 },
+  { id: 2, message: 'Hi, how are you ?', likesCount: -155 },
+  { id: 3, message: 'Ты че забыл тут?', likesCount: 2000000 },
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App posts={posts} />
   </React.StrictMode>,
   document.getElementById('root')
 );
